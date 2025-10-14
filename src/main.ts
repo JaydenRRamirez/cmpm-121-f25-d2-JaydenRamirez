93001930e47be54f86c55bfa0b48558b31aa0070
JaydenRRamirez <156058380+JaydenRRamirez@users.noreply.github.com>
2025-10-14 21:07:49 +0000

    Removed previous example code and implemented the UI Layout. First, the style.css has been updated to set up the canvas, font, colors, borders, and other display elements. In main.ts, we add the title through an h1 element alongside the canvas. (Step 1 Complete.)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const h1 = document.createElement("h1");
+h1.textContent = "Sticker Sketchpad";
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+
+document.body.append(h1, canvas);
